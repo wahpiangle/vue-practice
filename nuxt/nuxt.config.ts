6d00e9c7c78e043a977b4ec4023b72ffd3b11d23
Quan Ming <116425066+wahpiangle@users.noreply.github.com>
2025-06-09 18:32:29 +0800

    update

---
+++ nuxt/nuxt.config.ts
@@ -0,0 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/image', '@nuxt/icon', '@nuxt/content'],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
